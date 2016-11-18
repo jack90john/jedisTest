095aed21146d6bd26755643b41da7edc47059862
jack <jack90john@hotmail.com>
2016-11-18 18:10:56 +0800

    删除无用行。

--- src/main/java/com/RedisApplication.java
+++ src/main/java/com/RedisApplication.java
@@ -4,7 +4,6 @@ import com.example.RedisUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cache.annotation.Cacheable;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
